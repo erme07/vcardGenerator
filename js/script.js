c669a4aca5bd2085ce38241bfefbf380e1cd2ee1
erme07 <erik.medina07@gmail.com>
2023-06-02 18:06:41 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 import utilities from './functions.js'
 
-const $buttons = document.querySelector(".button");
-
 document.addEventListener("change", (e) => {
   if (e.target.classList.contains("data__input"))
     utilities.checkData(e.target);
   else if (e.target.getAttribute("name") != "extension")
     utilities.updateQR()
 })
-$buttons.addEventListener("click", (e) => {
+document.addEventListener("click", (e) => {
   if (e.target.getAttribute("id") === "generateQr") {
     utilities.enableOptions();
     utilities.enableItem(utilities.$buttonDownload)
